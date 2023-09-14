0ecdd5575c4d84a8c3bc6ae69630f603258badad
Josue Velazquez <josuevelazquez0704@gmail.com>
2023-09-14 17:57:03 -0600

    ♻️ Methods, computed and props

---
+++ Chapter 3/practice/counter/atributes.js
@@ -0,0 +1,35 @@
+const Atributes = {
+    data() {
+        return {
+            count: parseInt(this.start)
+        }
+    },
+    template: '{{time()}} &nbsp;&nbsp; The counter is {{count}}  &nbsp;&nbsp; Double is: {{countX2}}',
+    created() {
+        setInterval(() => {
+            this.count += 1
+        }, 1000)
+    },
+    methods: {
+        time() {
+            var date= new Date()
+            var hour = date.getHours()
+            var min = date.getMinutes()
+            var sec = date.getSeconds()
+            if (hour < 10) hour = "0" + hour
+            if (min < 10) min = '0' + min
+            if (sec < 10) sec = '0' + sec
+            return "" + hour + ":" + min + ":" + sec+ " "
+        },
+    },
+    computed: {
+        countX2() {
+            return 2 * this.count
+        }
+    },
+    props: [
+        "start"
+    ]
+}
+
+export default Atributes
